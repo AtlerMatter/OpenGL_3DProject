33340418c65e69f5025725bde27a7d23989d879b
Diogo Chrystêllo <DICRY@vestas.com>
2024-08-13 13:13:20 +0100

    Updated .gitignore file to be less harcoded, more versatile in relation to the chosen name for the cmake build folder

---
+++ _BACKUP/BUILD_release.bat
@@ -0,0 +1,42 @@
+@echo off
+setlocal EnableDelayedExpansion
+goto Setup
+
+:Compile
+cl.exe /EHsc /MT /fp:precise /sdl /O2 /GL /Ob2 /MP /Iinclude /Isrc\\vendor /Fedebug\\%CurrDir%_debug.exe /Fodebug\\ /Fddebug\\ %file% lib\\*.c /link /MACHINE:X64 lib\\*.lib gdi32.lib user32.lib kernel32.lib shell32.lib
+
+::Commented out compiler options
+:: /Zi
+:: /nologo 
+
+::pause
+exit
+
+
+:Setup
+for /f "delims=" %%a in ('dir /s /b /a-d src\\*.cpp') do set "file=!file! "%%a""
+set file=%file:~1%
+
+for %%I in (.) do set CurrDir=%%~nI%%~xI
+
+goto set_msvc
+exit
+
+
+:set_msvc
+call :check_msvc "%PROGRAMFILES%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+call :check_msvc "%PROGRAMFILES(x86)%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+call :check_msvc "%PROGRAMFILES%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call :check_msvc "%PROGRAMFILES(x86)%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+echo "Couldn't find MSVC. Please ensure you have Visual Studio installed."
+pause
+exit
+
+
+:check_msvc
+IF EXIST %1 (
+	::set msvc=%1
+	call %1
+	goto Compile
+)
